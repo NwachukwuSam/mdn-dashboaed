0c3f6f1d426f8965b65688d1ad39cb7db96cc8bf
NwachukwuSam <nwachukwusamuel123@gmail.com>
2023-11-29 10:10:05 +0100

    Deployment  commit

--- src/dashboard/RightNav.jsx
+++ src/dashboard/RightNav.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import bar from "../assets/bar.jpg"
+import bar from "../assets/bar.png"
 import pie from "../assets/pie.png"
+import line from "../assets/line.png"
 
 function RightNav() {
     return (
@@ -10,8 +11,9 @@ function RightNav() {
             </div>
             <div className='h-[500px] rounded-3xl ml-[-300px] mt-[55px] w-[300px] bg-white shadow-md shadow-blue-800'>
                 {/* <BarChart chartData={LoanData}/> */}
-                <img src={bar} alt="bar" className=' border-none h-[150px] w-[250px] mt-[20px] ml-[10px] ' />
-                <img src={pie} alt="bar" className=' border-none h-[150px] w-[250px] mt-[40px] ml-[10px]  ' />
+                <img src={bar} alt="bar" className=' border-none h-[150px] w-[250px] mt-[10px] ml-[10px] ' />
+                <img src={pie} alt="bar" className=' border-none h-[150px] w-[250px] mt-[20px] ml-[10px]  ' />
+                <img src={line} alt="bar" className=' border-none h-[150px] w-[250px] mt-[10px] ml-[10px]  ' />
             </div>
             
     </div>
